e2a3f1601724f663fe9aec34491beb3ecec4ee9d
Johannes Gräger <jgraeger@posteo.de>
2022-06-01 14:41:00 +0200

    Typo
    Co-authored-by: isabel <isaschm@users.noreply.github.com>

--- main.go
+++ main.go
@@ -113,7 +113,7 @@ func handleEverythingElse(url url.URL) http.Handler {
 //	* If the http request body is empty, it gets replace by an empty json object.
 func sanitizeRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") != "" {
+		if r.Header.Get("Content-Type") == "" {
 			r.Header.Set("Content-Type", "application/json")
 		}
 
